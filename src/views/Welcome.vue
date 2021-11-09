d896513216a0c3cf591c068f83061f88c74ad556
Taka2401 <tkht2401@gmail.com>
2021-11-09 19:32:09 +0900

    add: コンポネント復習

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,18 +1,32 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <p v-if="isEnabled">{{ subtitle }}</p>
-    <button @dblclick="toggle">切り替え</button>
+    <TestComponent @toggle="toggle" message="コンポーネントにデータが渡されています" />
+    <p v-if="isEnabled">こんにちは</p>
   </div>
 </template>
 
 <script>
+import TestComponent from '../components/TestComponent.vue'
+
 export default {
+   components: {
+    TestComponent
+  },
   data () {
     return {
       title: '初めてのVue.jsアプリです！',
       subtitle: 'ようこそ',
       isEnabled: true
+    }
+  },
+  computed: {
+    text () {
+      if (this.isEnabled) {
+        return 'こんにちは！'
+      } else {
+        return 'さよなら！'
+      }
     }
   },
   methods: {
@@ -22,3 +36,10 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+  p {
+    font-size: 20px;
+    color: red;
+  }
+</style>
