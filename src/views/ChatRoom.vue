a69d41cf1dca04753d197a0c0e0f85b66d39b5eb
Taka2401 <tkht2401@gmail.com>
2021-11-10 20:50:07 +0900

    test: リアルタイムチャット

--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -2,16 +2,19 @@
   <div class="container">
     <Navbar />
     <ChatWindow :messages="messages" />
+    <NewChatForm @connectCable="connectCable" />
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar'
 import ChatWindow from '../components/ChatWindow'
+import NewChatForm from '../components/NewChatForm'
 import axios from 'axios'
+import ActionCable from 'actioncable'
 
 export default {
-  components: { Navbar, ChatWindow },
+  components: { Navbar, ChatWindow, NewChatForm },
   data () {
     return {
       messages: [],
@@ -35,9 +38,27 @@ export default {
         console.log(err)
       }
     },
+    // $emitでmessageデータが引数として渡される
+    connectCable (message) {
+      this.messageChannel.perform('receive', {
+        message: message,
+        email: window.localStorage.getItem('uid')
+      })
+    }
   },
   mounted() {
-    this.getMessages()
+    const cable = ActionCable.createConsumer('ws://localhost:3000/cable')
+    this.messageChannel = cable.subscriptions.create('RoomChannel', {
+      connected: () => {
+        this.getMessages()
+      },
+      received: () => {
+        this.getMessages()
+      }
+    })
+  },
+  beforeUnmount () {
+    this.messageChannel.unsubscribe()
   }
 }
 </script>
