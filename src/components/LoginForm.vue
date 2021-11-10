0091abf4660e1e3cec50926a4abe87142705b80b
Taka2401 <tkht2401@gmail.com>
2021-11-10 18:21:15 +0900

    test: ログイン

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -5,16 +5,45 @@
       <input type="email" required placeholder="メールアドレス" v-model="email">
       <input type="password" required placeholder="パスワード" v-model="password">
       <button>ログインする</button>
+      <div class="error">{{ error }}</div>
     </form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+  emits: ['redirectToChatRoom'],
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
+    }
+  },
+    methods: {
+    async login() {
+      try {
+        this.error = null
+
+        const res = await axios.post('http://localhost:3000/auth/sign_in', {
+          email: this.email,
+          password: this.password,
+          }
+        )
+        if (!res) {
+          throw new Error('メールアドレスかパスワードが違います')
+        }
+        if (!this.error) {
+          this.$emit('redirectToChatRoom')
+        }
+        console.log({ res })
+        return res
+      } catch (error) {
+        console.log({ error })
+        this.error = 'メールアドレスかパスワードが違います'
+      }
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from "../views/Welcome";
+import Chatroom from '../views/ChatRoom'
 
 const routes = [
   {
     path: '/',
     name: 'Welcome',
     component: Welcome
+  },
+  {
+    path: '/chatroom',
+
+    // this.$router.pushの名前と一緒
+    name: 'Chatroom',
+    component: Chatroom
   }
 ]
 
