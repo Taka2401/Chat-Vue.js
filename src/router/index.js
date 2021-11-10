f38cb04a69de68dea9bdb07a37cd1c40b2ed458a
Taka2401 <tkht2401@gmail.com>
2021-11-10 19:32:07 +0900

    test: アクセス制限

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,64 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from "../views/Welcome";
 import Chatroom from '../views/ChatRoom'
+import useValidate from '../auth/validate'
+
+// useValidateを実行
+const { error, validate } = useValidate()
+
+const requireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('access-token')
+
+  if (!uid || !client || !accessToken) {
+    console.log('ログインしていません')
+    next({ name: 'Welcome' })
+    return
+  }
+
+  await validate()
+  if (error.value) {
+    console.log('認証に失敗しました')
+    next({ name: 'Welcome' })
+  } else {
+    next()
+  }
+}
+
+const noRequireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('access-token')
+
+  if (!uid && !client && !accessToken) {
+    next()
+    return
+  }
+
+  await validate()
+
+  if (!error.value) {
+    next({ name: 'Chatroom' })
+  } else {
+    next()
+  }
+}
 
 const routes = [
   {
     path: '/',
     name: 'Welcome',
-    component: Welcome
+    component: Welcome,
+    beforeEnter: noRequireAuth
   },
   {
     path: '/chatroom',
 
     // this.$router.pushの名前と一緒
     name: 'Chatroom',
-    component: Chatroom
+    component: Chatroom,
+    beforeEnter: requireAuth
   }
 ]
 
